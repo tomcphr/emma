1c197769656b107d9004dfc1be58c326e89a542f
tomcphr <tomcphr@pm.me>
2019-03-07 18:31:54 +0000

    No. 1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Game from "./objects/Game";
+
+window.onload = new Game();
---
+++ src/objects/Game.js
@@ -0,0 +1,8 @@
+import Phaser from "phaser";
+
+class Game extends Phaser.Game {
+    constructor () {
+        alert("Hello world");
+    };
+};
+export default Game;
