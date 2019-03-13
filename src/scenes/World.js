1e7bb51e8435fae8dd63f78f26ea4ffdc21afa08
tomcphr <tomcphr@pm.me>
2019-03-13 20:49:56 +0000

    Errm

--- src/scenes/World.js
+++ src/scenes/World.js
@@ -3,42 +3,70 @@ import Dungeon from "@mikewesthad/dungeon";
 export default class World extends Phaser.Scene
 {
     constructor () {
-        super({
-            "active": true
-        });
+        super();
+        this.floor = 0;
+    };
+
+    preload () {
+        let assets = "../../assets";
+        this.load.image("tiles", assets + "/tilesets/dungeon.png");
     };
 
     create () {
-        const dungeon = new Dungeon({
-            // The dungeon's grid size
-            width: 40,
-            height: 40,
+        this.dungeon = new Dungeon({
+            width: 50,
+            height: 50,
+            doorPadding: 2,
             rooms: {
-                // Random range for the width of a room (grid units)
-                width: {
-                    min: 5,
-                    max: 10
-                },
-                // Random range for the height of a room (grid units)
-                height: {
-                    min: 8,
-                    max: 20
-                },
-                // Cap the area of a room - e.g. this will prevent large rooms like 10 x 20
-                maxArea: 150,
-                // Max rooms to place
-                maxRooms: 10
-          }
+                width: {min: 7, max: 15, onlyOdd: true},
+                height: {min: 7, max: 15, onlyOdd: true}
+            }
         });
 
-        const html = dungeon.drawToHtml({
-          empty: " ",
-          wall: "W",
-          floor: "F",
-          door: "D"
+        // Creating a blank tilemap with dimensions matching the dungeon
+        const map = this.make.tilemap({
+            tileWidth: 48,
+            tileHeight: 48,
+            width: this.dungeon.width,
+            height: this.dungeon.height
         });
 
-        // Append the element to an existing element on the page
-        document.body.appendChild(html);
+        const tileset = map.addTilesetImage("tiles", null, 32, 32);
+        this.boundaries = map.createBlankDynamicLayer("boundaries", tileset);
+        this.interactable = map.createBlankDynamicLayer("interactable", tileset);
+
+        // Set all tiles in the boundaries layer with blank tiles (purple-black tile)
+        this.boundaries.fill(1);
+
+        this.dungeon.rooms.forEach(room => {
+            //this.createRoom(room);
+        });
     };
+
+    update () {
+    };
+
+    generate (room) {
+      // These room properties are all in grid units (not pixels units)
+      const { x, y, width, height, left, right, top, bottom } = room;
+
+      // Fill the room (minus the walls) with mostly clean floor tiles (90% of the time), but
+      // occasionally place a dirty tile (10% of the time).
+      this.boundaries.weightedRandomize(x + 1, y + 1, width - 2, height - 2, [
+        { index: 6, weight: 9 }, // 9/10 times, use index 6
+        { index: [7, 8, 26], weight: 1 } // 1/10 times, randomly pick 7, 8 or 26
+      ]);
+
+      // Place the room corners tiles
+      this.boundaries.putTileAt(1, left, top);
+      this.boundaries.putTileAt(4, right, top);
+      this.boundaries.putTileAt(23, right, bottom);
+      this.boundaries.putTileAt(22, left, bottom);
+
+      // Place the non-corner wall tiles using fill with x, y, width, height parameters
+      this.boundaries.fill(39, left + 1, top, width - 2, 1); // Top
+      this.boundaries.fill(1, left + 1, bottom, width - 2, 1); // Bottom
+      this.boundaries.fill(21, left, top + 1, 1, height - 2); // Left
+      this.boundaries.fill(19, right, top + 1, 1, height - 2); // Right
+    }
 }
