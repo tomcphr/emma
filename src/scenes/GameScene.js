a828d2040e426b707c10e7195e65941289f70882
tomcphr <tomcphr@pm.me>
2019-04-30 20:10:01 +0100

    Added support for NPCs

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,6 @@
 import Player from "../objects/Player";
 import World from "../objects/World";
+import Wanderer from "../objects/Npcs/Wanderer";
 
 export default class GameScene extends Phaser.Scene
 {
@@ -17,20 +18,32 @@ export default class GameScene extends Phaser.Scene
     };
 
     create() {
+        this.keys = this.input.keyboard.createCursorKeys();
+
         this.world = new World(this);
         this.world.generate();
         this.world.getFirstRoom().setAlpha(1);
 
+        this.boundaries = this.world.getBoundariesLayer();
+        this.boundaries.setCollisionByExclusion([2]);
+
+        this.npcs = this.add.group();
+
         let spawnX = this.world.getFirstRoom().getSpawnX();
         let spawnY = this.world.getFirstRoom().getSpawnY();
-        this.player = new Player(this, spawnX, spawnY);
 
-        let boundaries = this.world.getBoundariesLayer();
-        boundaries.setCollisionByExclusion([2]);
-        this.physics.add.collider(this.player.sprite, boundaries);
+        this.player = new Player(this, spawnX, spawnY);
+        this.physics.add.collider(this.player, this.npcs, (player, npc) => {
+            this.input.keyboard.off("keydown-SPACE");
+            this.input.keyboard.on("keydown-SPACE", () => {
+                npc.interact();
+            });
+        });
+
+        this.wanderer = new Wanderer(this, spawnX + 32, spawnY + 32, "player");
 
         const camera = this.cameras.main;
-        camera.startFollow(this.player.sprite);
+        camera.startFollow(this.player);
         camera.setBounds(0, 0, this.world.getTileMap().widthInPixels, this.world.getTileMap().heightInPixels);
     };
 
